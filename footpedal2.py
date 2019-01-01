3b510c6d19d1310c0fdfcaa3b98081c44abda1f6
Jukka Ylitalo <juylitalo@gmail.com>
2019-01-01 11:44:13 +0200

    wörkkaa taas

--- footpedal2.py
+++ footpedal2.py
@@ -10,22 +10,43 @@ import time,os
 from datetime import datetime,timedelta
 import re
 import pyautogui
+import platform
+from utils import paste_from_clipboard
 
-
+ossystem=platform.system()
 # try:
+
 print "Activating foot switches.."
+portnum=0
 
-for portnum in range(0,7):
+if ossystem != 'Windows':
+    for portnum in range(0,7):
 
-    try:
-        ser = serial.Serial(portnum)
-        print "Pic in port ", portnum
-        break
-    except:
-        pass
-#     ser.close()
-#     ser = serial.Serial(3)
+        try:
+            ser = serial.Serial(portnum)
+            ser = serial.Serial(portnum)
+            print "Pic in port ", portnum
+            break
+        except:
+            pass
+    #     ser.close()
+    #     ser = serial.Serial(3)
 
+
+else:
+
+    serport="COM5"
+    serports=["COM1","COM2","COM3","COM4","COM5","COM6","COM7","COM8","COM9","COM10","COM11","COM12"]
+    for serport in serports:
+        try:
+            print("tryin port ", serport)
+            ser = serial.Serial(serport)
+            break
+        except:
+            pass
+
+print "connect serialport",serport
+ser
 time.sleep(1)
 instream=""
 
@@ -39,6 +60,9 @@ last_values = ('0','0')
 while True:
 #for x in range(500):
 
+    # cntrlcb = paste_from_clipboard()
+    # if 'cancel' in cntrlcb:
+    #     print "cancelled"
     while ser.inWaiting() > 0:
         instream +=ser.read()
     instream = instream[-100:]
--- pedals_launcher.bat
+++ pedals_launcher.bat
@@ -1,1 +1,2 @@
-C:\Users\Jukka\Anaconda2\python.exe "E:\arduino\footpedal\Foot pedals as pc controllers\footpedal2.py"
+path=C:\Users\Jukka\Anaconda2;C:\Users\Jukka\Anaconda2\Scripts;C:\Users\Jukka\Anaconda2\Library\bin;%path%
+python "D:\py\Foot pedals as pc controllers\footpedal2.py"
