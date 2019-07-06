1db09c9fb754c25908708232c98d59d8737803a6
Jukka Ylitalo <juylitalo@gmail.com>
2019-07-06 10:50:54 +0300

    updating readme
    deleting redundant files
    rename main py file
    updating readme

--- pedals_launcher.bat
+++ pedals_launcher.bat
@@ -1,4 +1,4 @@
 echo off
 path=C:\Users\Jukka\Anaconda2;C:\Users\Jukka\Anaconda2\Scripts;C:\Users\Jukka\Anaconda2\Library\bin;%path%
 REM python "D:\py\Foot pedals as pc controllers\footpedal2.py"
-python "D:\py\Foot pedals as pc controllers\piezofootpedal2b.py"
+python "D:\py\Foot pedals as pc controllers\piezofootpedals.py"
